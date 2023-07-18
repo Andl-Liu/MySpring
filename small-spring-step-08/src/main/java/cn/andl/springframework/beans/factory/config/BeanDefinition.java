d89622547101400902dd082ce9f2f460f9a0bddb
andl <619246032@qq.com>
2023-07-18 15:26:29 +0800

    开始第九章：Aware感知容器对象

---
+++ small-spring-step-08/src/main/java/cn/andl/springframework/beans/factory/config/BeanDefinition.java
@@ -0,0 +1,61 @@
+package cn.andl.springframework.beans.factory.config;
+
+import cn.andl.springframework.beans.PropertyValues;
+
+/**
+ * Bean的定义类
+ */
+public class BeanDefinition {
+
+    private Class<?> beanClass;
+
+    private PropertyValues propertyValues;
+
+    // 初始化方法的名字
+    private String initMethodName;
+
+    // 销毁方法的名字
+    private String destroyMethodName;
+
+    public BeanDefinition(Class<?> beanClass) {
+        this.beanClass = beanClass;
+        this.propertyValues = new PropertyValues();
+    }
+
+    public BeanDefinition(Class<?> beanClass, PropertyValues propertyValues) {
+        this.beanClass = beanClass;
+        this.propertyValues = propertyValues == null ? new PropertyValues() : propertyValues;
+    }
+
+    public PropertyValues getPropertyValues() {
+        return propertyValues;
+    }
+
+    public void setPropertyValues(PropertyValues propertyValues) {
+        this.propertyValues = propertyValues;
+    }
+
+    public Class<?> getBeanClass() {
+        return beanClass;
+    }
+
+    public void setBeanClass(Class<?> beanClass) {
+        this.beanClass = beanClass;
+    }
+
+    public String getInitMethodName() {
+        return initMethodName;
+    }
+
+    public void setInitMethodName(String initMethodName) {
+        this.initMethodName = initMethodName;
+    }
+
+    public String getDestroyMethodName() {
+        return destroyMethodName;
+    }
+
+    public void setDestroyMethodName(String destroyMethodName) {
+        this.destroyMethodName = destroyMethodName;
+    }
+}
